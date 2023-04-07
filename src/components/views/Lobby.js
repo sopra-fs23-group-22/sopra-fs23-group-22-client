b076af2aeb14788e54a5172989abc8ccffedba25
rChenfeiXiong <113922300+rChenfeiXiong@users.noreply.github.com>
2023-04-07 23:12:23 +0200

    update lobby page
    add online user list; add search bar; add router when login and register;

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -1,7 +1,8 @@
 import 'styles/views/Lobby.scss'
 import 'styles/views/Game.scss'
 import Frame from "../ui/Frame";
-// import {Button} from "../ui/Button";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
 import {Spinner} from "../ui/Spinner";
 import {useHistory} from "react-router-dom";
 import {useEffect, useState} from "react";
@@ -31,43 +32,43 @@ const Myself = ({user}) => (
 Myself.propTypes = {
     user: PropTypes.object
 };
-// const Myself = ({user}) => (
-//     <div className="lobby user-myself-username">{user.username}</div>
-// );
-// const onlineUsers = ({user}) => (
-//     <div>
-//         <div className="lobby user-list-username">{user.username}</div>
-//         {/* <span>
-//           <a href=""> <img scr="" alt=""> </img> </a>
-//         </span> */}
-//     </div>
 
-// );
 const Lobby = props => {
-    // use react-router-dom's hook to access the history
+
     const history = useHistory();
 
-    // define a state variable (using the state hook).
-    // if this variable changes, the component will re-render, but the variable will
-    // keep its value throughout render cycles.
-    // a component can have as many state variables as you like.
-    // more information can be found under https://reactjs.org/docs/hooks-state.html
-    //test message
     const [myself, setMyself] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState(null);
     const [users, setUsers] = useState(null);
-    // const [user, setUser] = useState(null);
-    // const [friends, setFriends] = useState(null);
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = users.filter((value) => {
+            return value.username.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    };
+
     const logout = () => {
         localStorage.removeItem('token');
         history.push('/login');
     }
     useEffect(() => {
         // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function fetch() {
+        async function fetchMyself() {
             try {
                 // const userId = JSON.parse(localStorage.getItem('id'));
                 const userId = localStorage.getItem('id');
@@ -95,14 +96,43 @@ const Lobby = props => {
             }
         }
 
-        fetch();
+        fetchMyself();
 
     }, []);
     useEffect(() => {
         // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function fetchData() {
+        async function fetchOnlineUsers() {
             try {
                 const response = await api.get('/users/online');
+
+                // delays continuous execution of an async operation for 1 second.
+                // This is just a fake async call, so that the spinner can be displayed
+                // feel free to remove it :)
+                await new Promise(resolve => setTimeout(resolve, 1000));
+
+                // Get the returned users and update the state.
+                setOnlineUsers(response.data);
+
+                // This is just some data for you to see what is available.
+                // Feel free to remove it.
+
+                // See here to get more data.
+
+            } catch (error) {
+                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while fetching the users! See the console for details.");
+            }
+        }
+
+        fetchOnlineUsers();
+
+    }, []);
+    useEffect(() => {
+        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+        async function fetchUsers() {
+            try {
+                const response = await api.get('/users');
 
                 // delays continuous execution of an async operation for 1 second.
                 // This is just a fake async call, so that the spinner can be displayed
@@ -124,86 +154,61 @@ const Lobby = props => {
             }
         }
 
-        fetchData();
+        fetchUsers();
 
     }, []);
 
-    // useEffect(() => {
-    //     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-    //     async function fetch() {
-    //         try {
-    //             const userId = localStorage.getItem('id');
-    //             const response = await api.get('/users/{userId}');
-
-    //             // delays continuous execution of an async operation for 1 second.
-    //             // This is just a fake async call, so that the spinner can be displayed
-    //             // feel free to remove it :)
-    //             await new Promise(resolve => setTimeout(resolve, 1000));
-
-    //             // Get the returned users and update the state.
-    //             setMyself(response.data);
-
-    //             // This is just some data for you to see what is available.
-    //             // Feel free to remove it.
-
-    //             // See here to get more data.
-
-    //         } catch (error) {
-    //             console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
-    //             console.error("Details:", error);
-    //             alert("Something went wrong while fetching the users! See the console for details.");
-    //         }
-    //     }
-
-    //     fetch();
-
-    // }, []);
-
-
-
-    // let online = <div className="lobby user"> </div>;
     let content = <Spinner/>;
-    let content2 = <Spinner/>;
-    // if (users) {
-    //     content = (
-    //         <div className= "lobby user-list-username">
-    //             <ul className='lobby online-user-list'>
-    //             {users.map(user => (
-    //             <onlineUsers user={user} key={user.id}/>
-    //             ))}
-    //             </ul>
-    //       </div>
-    //       );
-    // }
+    // let content2 = <Spinner/>;
 
-
-    if (users && myself) {
+    if (onlineUsers && myself) {
         content = (
         <div className="lobby online-users-list">
             <Myself user={myself} key={myself.id}/>
             <ul>
-              {users.map(user => (
+              {onlineUsers.map(user => (
                 <OnlineUsers user={user} key={user.id}/>
               ))}
             </ul>
         </div>
         );
     }
-    if (myself) {
-        content2 = (
-        <div className="lobby online-users-myself">
-            <ul>
-                <Myself user={myself} key={myself.id}/>
-            </ul>
-        </div>
-        );
-    }
+    // if (myself) {
+    //     content2 = (
+    //     <div className="lobby online-users-myself">
+    //         <ul>
+    //             <Myself user={myself} key={myself.id}/>
+    //         </ul>
+    //     </div>
+    //     );
+    // }
     return (
         <div className="lobby row">
             <div className="lobby left">
-                <button className="lobby left-search-user">
-                    Search User
-                </button>
+                <div className="lobby left-search-user">
+                    <input className="lobby left-search-input"
+                    type="text"
+                    placeholder="Enter a Username"
+                    value={wordEntered}
+                    onChange={handleFilter}
+                    />
+                    <div className="lobby left-search-icon">
+                        {filteredData.length === 0 ? (
+                            <SearchIcon />
+                        ) : (
+                            <CloseIcon id="clearBtn" onClick={clearInput} />
+                        )}
+                    </div>
+                </div>
+                {filteredData.length != 0 && (
+                    <div className="lobby dataResult">
+                        {filteredData.slice(0, 15).map((value, key) => {
+                            return (
+                                <div className="lobby dataItem">{value.username}</div>
+                            );
+                        })}
+                    </div>
+                )}
                 <div className="lobby left-down-side">
                     <div className="lobby online-users-container">
                         <div className="lobby online-users-title">
