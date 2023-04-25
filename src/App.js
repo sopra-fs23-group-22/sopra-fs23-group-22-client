f18cd5082e25d191f6a4f9f3780c4d8ed0ce4b1c
stefanpluess <stefan.pluess@uzh.ch>
2023-04-25 18:01:54 +0200

    basic websocket implementation added

--- src/App.js
+++ src/App.js
@@ -8,15 +8,37 @@ import AppRouter from "components/routing/routers/AppRouter";
  * React Template by Lucas Pelloni
  * Overhauled by Kyrill Hux
  */
+import React, { useState } from 'react';
+import SockJsClient from 'react-stomp';
+
+const SOCKET_URL = 'http://localhost:8080/ws-message';
+
 const App = () => {
-  return (
-    <div style={{height: "100vh"}}>
-      {/* <Frame> */}
-        {/* <Header height="100"/> */}
-        <AppRouter/>
-      {/* </Frame> */}
-    </div>
-  );
-};
+    const [message, setMessage] = useState('You server message here.');
+
+    let onConnected = () => {
+        console.log("Connected!!")
+    }
+
+    let onMessageReceived = (msg) => {
+        setMessage(msg.message);
+    }
+
+    return (
+        <div>
+            <SockJsClient
+                url={SOCKET_URL}
+                topics={['/topic/message']}
+                onConnect={onConnected}
+                onDisconnect={console.log("Disconnected!")}
+                onMessage={msg => onMessageReceived(msg)}
+                debug={false}
+            />
+            <div>{message}</div>
+        </div>
+    );
+}
+
+
 
 export default App;
