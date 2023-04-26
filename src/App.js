33967676e51e0a5597d46664d089a5fc8039cf87
cincindydy <jiachen.cindy.bao@gmail.com>
2023-04-26 17:01:24 +0200

    Revert "Merge branch 'Websocket-test' into layout-update"
    This reverts commit 4a1b7615c14e7e37cf912c48b476c09d0844d2b2, reversing
    changes made to 6aa26c60d0530809ecaba4e64fa01c5d95432c1e.

--- src/App.js
+++ src/App.js
@@ -8,37 +8,15 @@ import AppRouter from "components/routing/routers/AppRouter";
  * React Template by Lucas Pelloni
  * Overhauled by Kyrill Hux
  */
-import React, { useState } from 'react';
-import SockJsClient from 'react-stomp';
-
-const SOCKET_URL = 'http://localhost:8080/ws-message';
-
 const App = () => {
-    const [message, setMessage] = useState('You server message here.');
-
-    let onConnected = () => {
-        console.log("Connected!!")
-    }
-
-    let onMessageReceived = (msg) => {
-        setMessage(msg.message);
-    }
-
-    return (
-        <div>
-            <SockJsClient
-                url={SOCKET_URL}
-                topics={['/topic/message']}
-                onConnect={onConnected}
-                onDisconnect={console.log("Disconnected!")}
-                onMessage={msg => onMessageReceived(msg)}
-                debug={false}
-            />
-            <div>{message}</div>
-        </div>
-    );
-}
-
-
+  return (
+    <div style={{height: "100vh"}}>
+      {/* <Frame> */}
+        {/* <Header height="100"/> */}
+        <AppRouter/>
+      {/* </Frame> */}
+    </div>
+  );
+};
 
 export default App;
