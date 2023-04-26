eb7f13c46ec691be9286a353999d6e786a4f8b39
cincindydy <jiachen.cindy.bao@gmail.com>
2023-04-27 01:29:25 +0200

    fix board render bug

--- src/components/views/OngoingGame.js
+++ src/components/views/OngoingGame.js
@@ -17,34 +17,13 @@ let pieceBeingDragged = null;
 
 const Board = ({targetBoard}) => {
 
-  const [boardToConvert, setBoardToConvert] = useState(targetBoard);
-
   let sourceSquare = null;
 
   const handlePieceDragStart = (e) => {
-    // console.log("dragging");
     pieceBeingDragged = e.target;
-    // console.log(pieceBeingDragged);
     sourceSquare = e.target.closest(".square");
-    // console.log(sourceSquare);
-    // assigning the dragging piece coordiante
     draggingStartCord = [e.target.parentNode.getAttribute("x"), e.target.parentNode.getAttribute("y")];
-    // console.log(draggingStartCord);
-    // console.log("drag start from: " + draggingStartCord);
   }
-
-  // useEffect(() => {
-  //   // console.log("running use effect");
-  //   async function fetchData() {
-  //     try {
-  //       const response = await api.get('/boards');
-  //       setBoard(response.data);
-  //     } catch(error) {
-  //       alert("Something went wrong while fetching the game! See the console for details.")
-  //     }
-  //   }
-  //   fetchData();
-  // }, [boardToConvert]);
 
 
   const handleSquareDragOver = (e) => {
@@ -70,7 +49,7 @@ const Board = ({targetBoard}) => {
       droppingTarget = [targetSquare.getAttribute("x"), targetSquare.getAttribute("y")]
       // prevent from attacking self
       if((droppingTarget[0]!== draggingStartCord[0])||(droppingTarget[1]!== draggingStartCord[1])){
-        sourceSquare.removeChild(pieceBeingDragged);
+        // sourceSquare.removeChild(pieceBeingDragged);
         sendMovingPiece(draggingStartCord, droppingTarget);
       }
     }
@@ -84,20 +63,11 @@ const Board = ({targetBoard}) => {
       console.log(`value: ${source} type: ${typeof(source)}`);
       try {
         const requestBody = JSON.stringify({source, target});
-        console.log(requestBody);
+        // console.log(requestBody);
         const response = await api.put("/boards", requestBody);
-        // ws.send(JSON.stringify({
-        //   method: 'PUT',
-        //   body: requestBody
-        // }));
-        // ws.onmessage = function(event) {
-        //   console.log('Received message from server: ' + event.data);
-        // };
-        console.log("send put request");
-        console.log(response);
-        console.log("request body is: " + requestBody);
-        // console.log(response.requestBody.requestURL);
-        // console.log(typeof(source));
+        // console.log("send put request");
+        // console.log(response);
+        // console.log("request body is: " + requestBody);
       } catch(error) {
         console.error(`Something went wrong while fetching the opponent: \n${handleError(error)}`);
         console.error("Details:", error);
@@ -107,61 +77,73 @@ const Board = ({targetBoard}) => {
 
   const onMessage = (msg) => {
     console.log(msg);
+    targetBoard = msg;
+    setGameBoard(convertBoardDTOtoBoard(convertToSquares(msg)));
     // setBoard(msg);
-    setBoardToConvert(msg);
+    // setBoardToConvert(msg);
   }
 
 
 
-  let gameBoard = [];
+  // const gameBoard = [];
+  const [gameBoard, setGameBoard] = useState(convertBoardDTOtoBoard(targetBoard));
+  // useEffect(()=> {
+  //   console.log(gameBoard);
+  //   convertBoardDTOtoBoard(targetBoard);
+  // },[gameBoard])
 
-  for(let i=0; i<10; i++) {
-      for(let j=0; j<10; j++) {
-          const pieceId = j + i * 10;
-          const targetPiece = boardToConvert[pieceId];
+
+  // showBoard(gameBoard);
+
+    // show board component based on the board received
+    function convertBoardDTOtoBoard(targetBoard) {
+      let boardToRender = [];
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          const pieceId = j+ i * 10;
+          const targetPiece = targetBoard[pieceId];
           const pieceType = targetPiece.piece.pieceType.toLowerCase();
           const army = targetPiece.piece.armyType.toLowerCase();
           let piece = null;
-
-          if(targetPiece.type==="BATTLE_FIELD") {
-            piece = pieceType !== null? 
-            (<Piece
-              id = {`piece-${pieceId}`}
-              class = 'piece'
-              type={pieceType} 
-              army={army} 
-              draggable
-              onDragStart = {handlePieceDragStart}
-              />) 
-                : null;
-            gameBoard.push(
-              <Square 
+  
+          if (targetPiece.type === "BATTLE_FIELD") {
+            piece = pieceType !== null ?
+              (<Piece
+                id={`piece-${pieceId}`}
+                class='piece'
+                type={pieceType}
+                army={army}
+                draggable
+                onDragStart={handlePieceDragStart} />)
+              : null;
+              boardToRender.push(
+              <Square
                 class="square"
                 key={pieceId}
                 value={i + j + 2}
                 content={piece}
-                onDragOver = {handleSquareDragOver}
-                onDrop = {handleSquareDrop}
+                onDragOver={handleSquareDragOver}
+                onDrop={handleSquareDrop}
                 // squareid={[i, j]}
-                x = {`_${i}`}
-                y = {`_${j}`}
-              />
+                x={`_${i}`}
+                y={`_${j}`} />
             );
           } else {
-            gameBoard.push(
-              <Square 
+            boardToRender.push(
+              <Square
                 key={pieceId}
                 value={i + j + 2}
                 content={piece}
                 type={"LAKE"}
                 // squareid={[i, j]}
-                x = {`_${i}`}
-                y = {`_${j}`}
-              />
+                x={`_${i}`}
+                y={`_${j}`} />
             );
           }
         }
-  }
+      }
+      return boardToRender;
+    }
 
   return (
   <div className='board'>
@@ -173,6 +155,7 @@ const Board = ({targetBoard}) => {
   </div>
   );   
 };
+
 
 const OngoingGame = () => {
 
@@ -201,7 +184,7 @@ const OngoingGame = () => {
     // console.log("checking board")
     let convertedBoard = convertToSquares(board);
     // console.log(convertedBoard); // Check if convertedBoard is defined
-    content = <Board target={convertedBoard}/>
+    content = <Board targetBoard={convertedBoard}/>
   }
 
   return (
