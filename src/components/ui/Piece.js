304b3247d5d97046257aaf9df20e91e3748e6e00
cincindydy <jiachen.cindy.bao@gmail.com>
2023-05-03 18:12:35 +0200

    updating images

--- src/components/ui/Piece.js
+++ src/components/ui/Piece.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import '../../styles/ui/Piece.scss'
+import bomb from '../images/piece/stratego-bomb.png'
+import captain from '../images/piece/stratego-captain.png'
+import colonel from '../images/piece/stratego-colonel.png'
+import flag from '../images/piece/stratego-flag.png'
+import general from '../images/piece/stratego-general.png'
+import lieutenant from '../images/piece/stratego-lieutenant.png'
+import major from '../images/piece/stratego-major.png'
+import marshal from '../images/piece/stratego-marshal.png'
+import miner from '../images/piece/stratego-miner.png'
+import scout from '../images/piece/stratego-scout.png'
+import sergeant from '../images/piece/stratego-sergeant.png'
+import spy from '../images/piece/stratego-spy.png'
 
 const Piece = ({ type, onClick, army, draggable, onDragStart}) => {
 
@@ -7,17 +19,59 @@ const Piece = ({ type, onClick, army, draggable, onDragStart}) => {
         return null;
     }
 
-    const imgSrc = `/images/piece/stratego-${type}.png`;
+    let source = null;
+    switch(type) {
+        case "bomb":
+            source = bomb;
+            break;
+        case "captain":
+            source = captain;
+            break;
+        case "colonel":
+            source = colonel;
+            break;
+        case "flag":
+            source = flag;
+            break;
+        case "general":
+            source = general;
+            break;
+        case "lieutenant":
+            source = lieutenant;
+            break;
+        case "major":
+            source = major;
+            break;
+        case "marshal":
+            source = marshal;
+            break;
+        case "miner":
+            source = miner;
+            break;
+        case "scout":
+            source = scout;
+            break;
+        case "sergeant":
+            source = sergeant;
+            break;
+        case "spy":
+            source = spy;
+            break;
+        default:
+            source = null;
+    }
+
+    // const imgSrc = `../images/piece/stratego-${type}.png`;
 
 
     let piece = null;
     if(army==="red") {
         piece = <div className='piece image-container-red'>
-                    <img className = "piece icon" alt = {type} src={imgSrc} onClick={onClick} draggable="false"/>
+                    <img className = "piece icon" alt = {type} src={source} onClick={onClick} draggable="false"/>
                 </div>
     } else {
         piece = <div className='piece image-container-blue'>
-                    <img className = "piece icon" alt = {type} src={imgSrc} onClick={onClick} draggable="false"/>
+                    <img className = "piece icon" alt = {type} src={source} onClick={onClick} draggable="false"/>
                 </div>
     }
 
