28f26c07fee4b181f003ca2dd05f12eeeccec117
cincindydy <jiachen.cindy.bao@gmail.com>
2023-05-10 22:20:19 +0200

    update initial configuration of blue player, refactor game preparing code

--- src/components/views/GamePreparing.js
+++ src/components/views/GamePreparing.js
@@ -13,70 +13,60 @@ import StrategoSocket from 'components/socket/StrategoSocket';
 import PlayerList from "../ui/PlayerList";
 import RulePopUp from 'components/ui/RulePopUp';
 import CustomPopUp from 'components/ui/CustomPopUp';
-
-const pieceTypes = [[null, null, "LAKE", "LAKE", null, null,"LAKE", "LAKE", null, null], 
-                    ["marshal", "general", "colonel", "colonel", "major", "major", "major", "captain", "captain", "captain"], 
-                    ["captain", "lieutenant", "lieutenant", "lieutenant", "lieutenant", "sergeant", "sergeant", "sergeant", "sergeant", "miner"], 
-                    ["miner", "miner", "miner", "miner", "scout", "scout", "scout", "scout", "scout", "scout"],
-                    ["scout", "scout", "spy", "bomb", "bomb","bomb","bomb","bomb","bomb", "flag"]]
+import ConfigurationModel from 'models/ConfigurationModel';
 
 
 const DefaultBoard = props => {
 
-    const {army} = props;
+    const {army, pattern} = props;
     console.log(`army type: ${army}`);
+    const [selectedPiecePosition, setSelectedPiecePosition] = useState(null);
+    let positionToBeSwapped = null;
 
-  const [selectedPiecePosition, setSelectedPiecePosition] = useState(null);
-  let positionToBeSwapped = null;
-  // store temporary position
-  let temp = null;
+    const handlePieceClick = (row, col) => {
+        // if the first position is not selected yet
+        if (selectedPiecePosition===null) {
+        // assign the position to the variable
+            setSelectedPiecePosition([row,col]);
+        } else {
+        // store the position of the other selected piece
+            positionToBeSwapped = [row, col];
+            pattern.swapPieces(selectedPiecePosition, positionToBeSwapped);
+            setSelectedPiecePosition(null);
+        }
+    };
 
-  const handlePieceClick = (row, col) => {
-    // if the first position is not selected yet
-    if (selectedPiecePosition===null) {
-      // assign the position to the variable
-      setSelectedPiecePosition([row,col]);
-    } else {
-      // store the position of the other selected piece
-      positionToBeSwapped = [row, col];
-      // store the type of the other selected piece
-      temp = pieceTypes[positionToBeSwapped[0]][positionToBeSwapped[1]];
-      // swap the two piece
-      pieceTypes[positionToBeSwapped[0]][positionToBeSwapped[1]] = pieceTypes[selectedPiecePosition[0]][selectedPiecePosition[1]];
-      pieceTypes[selectedPiecePosition[0]][selectedPiecePosition[1]] = temp;
-      // set the first posistion to be null again
-      setSelectedPiecePosition(null);
+    const board = [];
+    const pieces = pattern.configuration;
+    console.log(pieces);
+    for (let i=0; i < 5; i++) {
+        for (let j=0; j < 10; j++) {
+            const pieceType = pieces[i][j];
+            if(pieceType==="LAKE") {
+                board.push(
+                    <Square
+                        key = {`${i}-${j}`}
+                        value = {i + j+ 2}
+                        type = {"LAKE"}
+                    />
+                )
+            } else {
+                const piece = pieceType !== null? 
+                <Piece
+                    type={pieceType}
+                    army={army}
+                    onClick={() => handlePieceClick(i,j)}
+                /> :null;
+                board.push(
+                    <Square 
+                        key = {`${i}-${j}`}
+                        value = {i + j+ 2}
+                        content={piece}
+                    />
+                );
+            }
+        }
     }
-  };
-
-  const board = [];
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 10; j++) {
-      const pieceType = pieceTypes[i][j];
-      if(pieceType==="LAKE") {
-        board.push(
-            <Square
-                key={`${i}-${j}`}
-                value={i + j + 2}
-                type={"LAKE"}
-            />
-        )
-      } else {
-        const piece = pieceType !== null ? 
-            <Piece 
-            type={pieceType} 
-            army={army} 
-            onClick={() => handlePieceClick(i, j)} 
-            /> : null;
-            board.push(
-            <Square 
-                key={`${i}-${j}`} 
-                value={i + j + 2} 
-                content={piece} 
-            />);
-    }
-    }
-  }
   
   return (
     <div className='pregame defaultBoard'>
@@ -87,21 +77,13 @@ const DefaultBoard = props => {
 
 
 
-// const convertPieceTypeToPiece = ({pieceTypes}) => {
-//   const gamePiece = new GamePiece(pieceTypes, "red");
-//   return gamePiece;
-// }
-
-
-
 const GamePreparing = () => {
 
     const {roomId, playerId} = useParams();
-
     const [rightContent, setRightContent] = useState(<Spinner/>);
     const history = useHistory();
     let gameState = null;
-
+    let configuration = null;
     let armyType = null;
 
     const onMessage = (msg) => {
@@ -113,17 +95,42 @@ const GamePreparing = () => {
       }
 
 
+    useEffect(() => {
+    async function fetchPlayers() {
+        console.log("use effect fetch players")
+        try {
+            const room = await api.get("/rooms/" + roomId);
+            const players = room.data.userIds;
+            const currentPlayer = localStorage.getItem("id");
+            if(currentPlayer===JSON.stringify(players[0])) {
+                armyType = "red";
+            } else if(currentPlayer===JSON.stringify(players[1])){
+                armyType = "blue";
+            }
+            configuration = new ConfigurationModel(armyType);
+            setRightContent(
+                <div className='pregame container'>
+                    <div className='pregame board-container'>
+                        <DefaultBoard army={armyType} pattern={configuration}/>
+                    </div>
+                    <div className='pregame confirm-button-container'>
+                        <button className="pregame confirm-button" 
+                                onClick={doConfirm}>
+                            Confirm
+                        </button>
+                    </div>  
+                </div>)
+        } catch (error) {
+            console.error(`Something went wrong while fetching the players: \n${handleError(error)}`);
+            console.error("Details:", error);
+            alert("Something went wrong while fetching the players! See the console for details.");
+        }}
+    fetchPlayers();
+    },[]);
+
     const doConfirm = async () => {
         try {
-            console.log(pieceTypes.length);
-            const board = [];
-            for(let i=1; i<pieceTypes.length; i++) {
-              for(let j=0; j<pieceTypes[i].length; j++) {
-                const gamePiece = new GamePiece(pieceTypes[i][j], armyType);
-                board.push(gamePiece);
-              }
-            }
-            const requestBody = JSON.stringify(board);
+            const requestBody = JSON.stringify(configuration.getPieces());
             const response = await api.put(`/rooms/${roomId}/setBoard`, requestBody);
             console.log(response);
             if(response.data === "PRE_PLAY") {
@@ -145,47 +152,6 @@ const GamePreparing = () => {
             alert("Something went wrong while sending the board! See the console for details.");
         }
     }
-
-        useEffect(() => {
-            async function fetchPlayers() {
-                console.log("use effect fetch players")
-                try {
-                    const room = await api.get("/rooms/" + roomId);
-                    const players = room.data.userIds;
-                    console.log(`player 1: ${JSON.stringify(players[0])}`);
-
-                    const currentPlayer = localStorage.getItem("id");
-                    console.log(currentPlayer===JSON.stringify(players[0]));
-                    console.log(`currentPlayer is ${currentPlayer}`);
-                    if(currentPlayer===JSON.stringify(players[0])) {
-
-                        console.log("enter here")
-                        // red player
-                        armyType = "red";
-                        console.log("after setting color: "+armyType);
-                    } else if(currentPlayer===JSON.stringify(players[1])){
-                        console.log("also enter here")
-                        armyType = "blue";
-                        console.log("after setting color: "+armyType);
-                    }
-                    console.log("after setting color: "+armyType);
-                    setRightContent(
-                        <div className='pregame container'>
-                            <div className='pregame board-container'>
-                                <DefaultBoard army={armyType}/>
-                            </div>
-                            <div className='pregame confirm-button-container'>
-                                <button className="pregame confirm-button" onClick={doConfirm}>Confirm</button>
-                            </div>  
-                        </div>)
-                } catch (error) {
-                    console.error(`Something went wrong while fetching the players: \n${handleError(error)}`);
-                    console.error("Details:", error);
-                    alert("Something went wrong while fetching the players! See the console for details.");
-                }
-            }
-            fetchPlayers();
-        },[]);
 
       return (
             <div className="lobby row">
---
+++ src/models/ConfigurationModel.js
@@ -0,0 +1,46 @@
+import GamePiece from "./GamePiece";
+
+class ConfigurationModel {
+    constructor(armyType) {
+      const redConfig = [
+        [null, null, "LAKE", "LAKE", null, null, "LAKE", "LAKE", null, null],
+        ["marshal", "general", "colonel", "colonel", "major", "major", "major", "captain", "captain", "captain"],
+        ["captain", "lieutenant", "lieutenant", "lieutenant", "lieutenant", "sergeant", "sergeant", "sergeant", "sergeant", "miner"],
+        ["miner", "miner", "miner", "miner", "scout", "scout", "scout", "scout", "scout", "scout"],
+        ["scout", "scout", "spy", "bomb", "bomb", "bomb", "bomb", "bomb", "bomb", "flag"]
+      ];
+
+      const blueConfig = this.mirror(redConfig);
+      this.armyType = armyType;
+      this.configuration = armyType === 'red' ? redConfig : blueConfig;
+    }
+  
+    swapPieces(source, target) {
+      const temp = this.configuration[source[0]][source[1]];
+      this.configuration[source[0]][source[1]] = this.configuration[target[0]][target[1]];
+      this.configuration[target[0]][target[1]] = temp;
+    }
+  
+    getPieces() {
+      const pieces = [];
+      for (let i = 0; i < this.configuration.length; i++) {
+        for (let j=0; j< this.configuration[i].length; j++) {
+            const type = this.configuration[i][j];
+            if(type !== null && type !== 'LAKE') {
+                pieces.push(new GamePiece(type, this.armyType));
+            }
+        }
+      }
+      return pieces;
+    }
+
+    mirror(pattern) {
+        const mirroredPattern = [];
+        for(let i=4; i>=0; i--) {
+            mirroredPattern.push(pattern[i].slice().reverse());
+        }
+        return mirroredPattern;
+    }
+  }
+
+  export default ConfigurationModel;
