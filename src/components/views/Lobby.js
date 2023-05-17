d83c3590daadc02d0c68fe1b71c92202d8fd63a7
cincindydy <jiachen.cindy.bao@gmail.com>
2023-05-17 10:43:39 +0200

    Revert "specific rulepopup for different pages"
    This reverts commit 9254944fdad2cd53acf570ea9c789d11b0ebba98.

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -9,10 +9,6 @@ import NavBar from "components/ui/NavBar";
 import {useEffect, useState} from "react";
 
 const Lobby = (props) => {
-  const gameResultRules = ['It is a 2-player strategy board game. Command your army and try to capture your opponent’s flag! ',
-   'You will have 33 movable pieces (1 Marshal, rank 10; 1 General, rank 9; 2 Colonels, rank 8; 3 Majors, rank 7; 4 Captains, rank 6; 4 Lieutenants, rank 5; 4 Sergeants, rank 4; 5 Miners, rank 3; 8 Scouts, rank 2; 1 Spy, rank S ) and 7 pieces that do not move (6 Bombs and 1 Flag).',
-    'Higher number indicates higher rank. Lower ranked pieces like the Miners, Scouts and Spy, have unique privileges.'];
-  const gameResultInformation = 'Welcome to Stratego!';
   const history = useHistory();
   const [roomIdOfUser, setRoomIdOfUser] = useState(null);
   const userId = localStorage.getItem("id");
@@ -59,7 +55,7 @@ const Lobby = (props) => {
         <NavBar renderLobbyBtn={false} renderLogoutBtn={true} />
         <div className="lobby right-main">
           <div className="lobby right-info-container">
-          <RulePopUp rules={gameResultRules} information={gameResultInformation} />
+            <RulePopUp />
           </div>
           <div className="lobby right-base-container">
 
